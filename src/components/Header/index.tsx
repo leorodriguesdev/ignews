b447b0cb035b26f3900cc0d6faef65566729c32d
Leonardo  Rodrigues <leonardo.rodrigues@ibm.com>
2021-10-20 04:08:30 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+    return (
+        <header>
+            <div>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Posts</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
